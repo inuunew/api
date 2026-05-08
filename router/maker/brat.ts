12b664effd5cee9cacd2b6e814549aae5fdfb67e
inuunew <choirulazzamibnu@gmail.com>
2026-05-08 13:45:28 +0700

    Update brat.ts

--- router/maker/brat.ts
+++ router/maker/brat.ts
@@ -5,10 +5,7 @@ export default async function bratHandler(req: Request, res: Response) {
     const text = (req.query.text || req.body.text) as string;
 
     if (!text) {
-        return res.status(400).json({
-            status: false,
-            message: "Parameter 'text' diperlukan."
-        });
+        return res.status(400).json({ status: false, message: "Parameter 'text' diperlukan." });
     }
 
     try {
@@ -16,10 +13,7 @@ export default async function bratHandler(req: Request, res: Response) {
         const response = await axios.get(url, {
             responseType: 'arraybuffer',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-                'Referer': 'https://brat.siputzx.my.id/',
-                'Connection': 'keep-alive'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36'
             }
         });
 
